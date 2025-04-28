1ae56eebd42b78914a1be271b0a8218960f01c5a
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-28 22:41:06 +0530

    third commit

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 
 const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
+  
   return (
-    <div
+    <div 
       className="tooltip"
       onMouseEnter={() => setShow(true)}
       onMouseLeave={() => setShow(false)}
     >
-      {children}
+      {React.cloneElement(children)}
       {show && <div className="tooltiptext">{text}</div>}
     </div>
   );
